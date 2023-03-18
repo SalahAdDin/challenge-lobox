7139b04ddada4d46e5db8cb8c68068a22a4e1f8e
José Luis <alagunasalahaddin@live.com>
2023-03-19 00:32:23 +0300

    chore: :coffin: Removing unused code.

--- src/main.tsx
+++ src/main.tsx
@@ -5,25 +5,10 @@ import App from "@presentation/App";
 
 import reportWebVitals from "./reportWebVitals";
 
-async function prepare() {
-  if (process.env.NODE_ENV === "development") {
-    const { default: worker } = await import("../mocks/browser");
-
-    return worker.start();
-  }
-
-  return Promise.resolve();
-}
-
-prepare().then(
-  () => {
-    ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-    );
-  },
-  () => {},
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 reportWebVitals();
