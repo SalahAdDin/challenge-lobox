d24f28fc628980db19449173960a58b769e0eebf
José Luis <alagunasalahaddin@live.com>
2023-03-18 20:42:46 +0300

    chore: :wastebasket: removing un-required code.

--- src/main.tsx
+++ src/main.tsx
@@ -2,32 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "@presentation/App";
-// import router from "@presentation/router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// import { RouterProvider } from "@tanstack/react-router";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-
-const queryClient = new QueryClient();
-
-const ReactQueryDevtools =
-  process.env.NODE_ENV === "production"
-    ? () => null
-    : React.lazy(() =>
-        import("@tanstack/react-query-devtools").then((res) => ({
-          default: res.ReactQueryDevtools,
-        }))
-      );
-
-/* const ReactRouterDevtools =
-  process.env.NODE_ENV === "production"
-    ? () => null
-    : React.lazy(() =>
-        import("@tanstack/react-router-devtools").then((res) => ({
-          default: res.TanStackRouterDevtools,
-        })),
-      ); */
 
 async function prepare() {
   if (process.env.NODE_ENV === "development") {
@@ -43,22 +19,11 @@ prepare().then(
   () => {
     ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-          <App />
-          {/*
-        <RouterProvider router={router} />
-         <ReactRouterDevtools
-          initialIsOpen={false}
-          router={router}
-          position="bottom-right"
-        />
-        */}
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </React.StrictMode>
+        <App />
+      </React.StrictMode>,
     );
   },
-  () => {}
+  () => {},
 );
 
 reportWebVitals();
--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,19 +1,12 @@
-// import { Outlet } from "@tanstack/react-router";
+import mockService from "@application/utils/mock";
+
+import DropdownList from "./components/DropdownList";
+
+const defaultOptions = mockService.generateMockedList();
 
 const App = () => (
-  <div className="container my-8 max-w-screen-lg">
-    {/*
-    // TODO: use this code as base of Layout component or similar.
-    <Suspense
-      fallback={
-        <div className="flex h-[75vh] w-full flex-col items-center justify-center">
-          <Spinner />
-        </div>
-      }
-    >
-      <Outlet />
-    </Suspense>
-    */}
+  <div>
+    <DropdownList options={defaultOptions} name="test" />
   </div>
 );
 
