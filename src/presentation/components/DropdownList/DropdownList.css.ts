f5a43e3dc93d7e5233e31a7d62a41f36008bd742
José Luis <alagunasalahaddin@live.com>
2023-03-20 01:53:00 +0300

    feat: :lipstick: Adding component styles and relocating component.

---
+++ src/presentation/components/DropdownList/DropdownList.css.ts
@@ -0,0 +1,87 @@
+import tokens from "@presentation/themes.css";
+import { style } from "@vanilla-extract/css";
+
+const dropdownContainer = style({});
+
+const dropdownInputContainer = style({
+  display: "flex",
+  position: "relative",
+  marginTop: tokens.space.sm,
+  marginBottom: tokens.space.sm,
+});
+
+const dropdownInput = style({
+  "width": "100%",
+  "border": `3px solid ${tokens.colors.gray}`,
+  ":focus-visible": {
+    outline: `5px solid ${tokens.colors.primary.hover}`,
+    border: `2px solid ${tokens.colors.primary.main}`,
+  },
+  "selectors": {
+    [`${dropdownInputContainer} &`]: {
+      padding: tokens.space.md,
+      borderRadius: tokens.radii.lg,
+    },
+  },
+});
+
+const dropdownDownArrowContainer = style({
+  selectors: {
+    [`${dropdownInputContainer} &`]: {
+      background: tokens.colors.background.white,
+      border: "none",
+      alignSelf: "center",
+      position: "absolute",
+      right: tokens.space.md,
+      padding: tokens.space.sm,
+    },
+  },
+});
+
+const dropdownMenuContainer = style({
+  overflow: "hidden",
+  border: `3px solid ${tokens.colors.gray}`,
+  borderRadius: tokens.radii.xl,
+  background: tokens.colors.background.white,
+  boxShadow: tokens.boxShadow.lg,
+});
+
+const dropdownMenu = style({
+  listStyleType: "none",
+  paddingTop: tokens.space.lg,
+  paddingBottom: tokens.space.lg,
+  paddingLeft: tokens.space.md,
+  paddingRight: tokens.space.md,
+  overflow: "auto",
+  maxHeight: tokens.space[80],
+});
+
+const dropdownOption = style({
+  "display": "flex",
+  "justifyContent": "space-between",
+  "padding": tokens.space.md,
+  "borderRadius": tokens.radii.lg,
+  ":hover": {
+    color: tokens.colors.primary.main,
+    background: tokens.colors.primary.hover,
+  },
+});
+
+const dropdownOptionCheck = style({
+  selectors: {
+    [`${dropdownOption} &`]: {
+      alignSelf: "center",
+    },
+  },
+});
+
+export default {
+  dropdownContainer,
+  dropdownDownArrowContainer,
+  dropdownInputContainer,
+  dropdownInput,
+  dropdownMenuContainer,
+  dropdownMenu,
+  dropdownOption,
+  dropdownOptionCheck,
+};
---
+++ src/presentation/components/DropdownList/DropdownList.tsx
@@ -0,0 +1,95 @@
+import { CSSProperties, ChangeEvent, FC, KeyboardEvent } from "react";
+
+import { ArrowDown2, ArrowUp2, TickCircle } from "iconsax-react";
+
+import useDynamicList from "@application/useDynamicList";
+import { ListItem } from "@domain/list.dto";
+
+import styles from "./DropdownList.css";
+
+interface DropdownListProps {
+  name: string;
+  options?: ListItem[];
+  style?: CSSProperties | undefined;
+}
+
+const DropdownList: FC<DropdownListProps> = ({ name, options = [], style }) => {
+  const [
+    items,
+    inputRef,
+    showMenu,
+    toggleList,
+    isSelectedItem,
+    inputValue,
+    setInputValue,
+    addItem,
+    setSelectedItem,
+  ] = useDynamicList(options);
+
+  const addItemOnKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+      key,
+    } = event;
+    if (key === "Enter") {
+      const newItem = {
+        label: value,
+        value: crypto.randomUUID(),
+      };
+
+      addItem(newItem);
+    }
+  };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = event;
+
+    setInputValue(value);
+  };
+
+  return (
+    <div className={styles.dropdownContainer} style={style}>
+      <div
+        ref={inputRef}
+        className={styles.dropdownInputContainer}
+        onClick={toggleList}>
+        <input
+          type="text"
+          placeholder="Enter a new option"
+          list={`dropdown-${name}`}
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyDown={addItemOnKeyDown}
+          className={styles.dropdownInput}
+        />
+        <span className={styles.dropdownDownArrowContainer}>
+          {showMenu ? <ArrowUp2 size="16" /> : <ArrowDown2 size="16" />}
+        </span>
+      </div>
+      {showMenu && (
+        <div className={styles.dropdownMenuContainer}>
+          <ul id={`dropdown-${name}`} className={styles.dropdownMenu}>
+            {items.map(({ value, label }) => (
+              <li
+                key={value}
+                onClick={() => setSelectedItem({ value, label })}
+                className={styles.dropdownOption}>
+                {label}
+                {isSelectedItem(value) && (
+                  <TickCircle
+                    size="16"
+                    className={styles.dropdownOptionCheck}
+                  />
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DropdownList;
